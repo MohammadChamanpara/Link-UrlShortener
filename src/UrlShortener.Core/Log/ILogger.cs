84c8c3831836711ac991c31c958722f35ffbabe8
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2017-08-27 20:44:45 +0800

    Add ILogger interface to the Core project
    In order to be injected as the logger to the functional classes
    in the application.

---
+++ src/UrlShortener.Core/Log/ILogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Core.Log
+{
+	public interface ILogger
+	{
+		void Debug(string message, params object[] messageParameters);
+		void Information(string message, params object[] messageParameters);
+		void Warning(string message, params object[] messageParameters);
+		void Error(string message, params object[] messageParameters);
+		void Error(Exception exception, string message, params object[] messageParameters);
+		void Fatal(string message, params object[] messageParameters);
+		void Fatal(Exception exception, string message, params object[] messageParameters);
+	}
+}
